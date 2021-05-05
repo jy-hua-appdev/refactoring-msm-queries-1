4b8aed73560430925a1cd8e6d4c2dbdc47db6a8c
jy-hua <jhuang17@chicagobooth.edu>
2021-05-05 20:05:50 +0000

    All complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,13 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  def director
+    matching_directors = Director.where({ :id => self.director_id })
+    
+    the_director = matching_directors.at(0)
+
+    return the_director
+  end
+
 end
